c3f154864f95e33cf68a79d4fa8668d1d16e5c8e
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-23 09:24:44 +0530

    Set and Map are added

--- _002_dataTypes.dart
+++ _002_dataTypes.dart
@@ -14,23 +14,50 @@
 */
 
 void main(){
-  int x = 10;
-  double y = 1.0;
+  int _int = 10;
+  double _double = 1.0;
 
   //num datatype can store both int and flaot.
-  num test = 2; 
-  num test2 = 4.5;
+  num _num_int = 2; 
+  num _num_double = 4.5;
   
+  //String data type
   String str = "Somnath Sardar";
-  bool dp = true;
+
+  //Boolean data type
+  bool _boolean = true;
+
+  //Simiral to array.
   List l1 = [1,2,3, "hello world"];
 
-  print(x);
-  print(y);
-  print(test);
-  print(test2);
+  //A set in Dart is an unordered collection of unique items
+  Set set1 = {"test1", 1, "Test2"};
+
+  //In general, a map is an object that associates keys and values.
+  Map map1 = {
+    "key1" : "Hello",
+    "key2" : "Hello again"
+  };
+
+  print("***********Number***********");
+  print(_int);
+  print(_double);
+  print(_num_int);
+  print(_num_double);
+  
+  print("\n*************String************");
   print(str);
-  print(dp);
+
+  print("\n*************Boolean************");
+  print(_boolean);
+
+  print("\n*************List************");
   print(l1);
 
+  print("\n*************Set************");
+  print(set1);
+
+  print("\n*************Map************");
+  print(map1);
+  print(map1['key1']);
 }
