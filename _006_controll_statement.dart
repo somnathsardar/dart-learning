1bb6095bd490bb8253a5cfab8431178cabc2eaea
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-23 12:32:37 +0530

    Controll statement file is added

---
+++ _006_controll_statement.dart
@@ -0,0 +1,28 @@
+void main()
+{
+  var name = "Somnath Sardar";
+
+  // if-else statement.
+  if(name == 'Somnath')
+  {
+    print("Matched");
+  }
+  else
+  {
+    print("Not matched");
+  }
+
+  //else if
+  if(name == 'Somnath')
+  {
+    print("Found in if block");
+  }
+  else if(name == 'Somnath Sardar')
+  {
+    print("Found in else if block");
+  }
+  else
+  {
+    print("Not found");
+  }
+}
