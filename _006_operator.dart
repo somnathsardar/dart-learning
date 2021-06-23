a29fa5935a8b43a3526b2dcaaedd73d82f0af9dd
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-23 12:47:08 +0530

    operator.dart file is created

---
+++ _006_operator.dart
@@ -0,0 +1,35 @@
+/* 
+  Tes of operator :-
+
+    1. Arithmetic operators
+      + => Addition
+      - => Subtraction
+      * => Multipliation
+      / => Division
+      % => Modulo
+    
+    2. Equality and relational operators
+      == => Equal
+      != => Not Equal
+      >  => Grater than
+      <  => Less than
+      >= => Grater Than or Equal
+      <= => Less Than or Equal
+    
+    3. Type test operators
+      as => Typecast
+      is => True if the object has the specified type
+      is! => True if the object doesn’t have the specified type
+
+    4. Logical operators
+      && => logical AND
+      || => logical OR
+
+    5. Ternary operator 
+      ? :
+*/
+
+void main()
+{
+  var x = 10
+}
