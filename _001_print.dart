141ad36a1298adb813ae8bb3aa9941544b1c95b4
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-23 08:48:33 +0530

    Print message in command line

---
+++ _001_print.dart
@@ -0,0 +1,12 @@
+/*
+  Every dart program starts execution from the main() method.
+  To run any dart program we need to write below command in the command line
+  
+  dart <file-name> 
+  
+*/
+
+void main()
+{
+  print("Hello World"); //Need to add ';' at the end or else it will give error.
+}
