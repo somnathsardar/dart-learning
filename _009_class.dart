3c50acc20e083841f3d284a63004b06429f921f9
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-23 14:20:19 +0530

    class.dart and nullCheck.dart files are added

---
+++ _009_class.dart
@@ -0,0 +1,15 @@
+class Test{
+  var x;
+  var y;
+
+  Test(int x,int y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+void main()
+{
+  var t = Test(2,5);
+  print(t);
+}
---
+++ _010_nullCheck.dart
@@ -0,0 +1,5 @@
+void main()
+{
+  var x;
+  print("It will print null in the console: ${x}");
+}
