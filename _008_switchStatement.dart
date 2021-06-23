5d60c7fd09226351fbcfea07fde93f4237689050
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-23 12:55:28 +0530

    switchStatement.dart file is created

---
+++ _008_switchStatement.dart
@@ -0,0 +1,18 @@
+/* 
+  Switch is similar to if else statement.
+*/
+void main()
+{
+  var num = 1;
+
+  switch(num){
+    case 1:
+      print("One");
+      break;
+    case 2:
+      print("Two");
+      break;
+    default:
+      print("None of the cases defined");
+  }
+}
