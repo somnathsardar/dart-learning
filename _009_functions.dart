6da095f02ab2d95d39fa78acf1fa5c7c64251508
Somnath Sardar <githubsomnath1996@gmail.com>
2021-06-24 13:44:38 +0530

    functions.dart file modified

--- _009_functions.dart
+++ _009_functions.dart
@@ -8,25 +8,41 @@
 
 void main()
 {
-  print("Function without parameter call: \n${getText()}");
-  print("Function without parameter call: \n${AddNumber(7, 9)}");
+  _function_without_parameter();
+  _function_with_parameter(7, 9);
+  _function_with_named_parameter(item1: 1, item2: 5);
+  _funtion_mix(2, s2:10, s1: 50);
   
   //Anonymous functions
-  print("\nAnonymous function");
+  print("\nThis is an anonymous function call");
   List fruits = ['Apple', 'Banana'];
   fruits.forEach((item) {
     print("${fruits.indexOf(item)} : ${item}");
   });
 }
 
-//Function without paraameter
-String getText()
+//Function without parameter
+void _function_without_parameter()
 {
-  return "Hello this is a function without parameter";
+  print("This is a function call without parameter");
 }
 
-//Function with parameters
-int AddNumber(int x, int y)
+//Function with positional parameters
+void _function_with_parameter(int x, int y)
 {
-  return (x+y);
+  print("\nThis is function call with positional parameter: ${x+y}");
+}
+
+//Function with named parameter
+void _function_with_named_parameter({var item1, var item2})
+{
+  print("\nThis is function call with named parameter. That means at the time of invocation, we need to mention the parameter name.");
+  print("item1: ${item1}\nitem2: ${item2}");
+}
+
+//function with positional and named parameter
+void _funtion_mix(var t1, {var s1, var s2})
+{
+  print("\nThis is a funcction call with positional and named parameter");
+  print("${t1}\n${s1}\n${s2}");
 }
